0035925b3555c2734906a4998684d08792f8db80
terraform user <kimcity0205@gmail.com>
2024-10-11 16:44:37 +0900

    file add

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "172.17.0.0/16"
+
+  tags = {
+    Name = "terraform_vpc"
+  }
+}
